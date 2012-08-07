f1268043cce17f33c4d734c0f3a5ccb946b57071
yukicoder <yukicoder@gmail.com>
2012-08-07 14:08:17 -0600

    Add static Help About Home pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,10 +3,15 @@ require 'spec_helper'
 describe "Static pages" do
 
   describe "Home page" do
-
-    it "should have the content 'Test App'"do
+    
+    it "should have the h1 'Test App'" do
       visit '/static_pages/home'
-      page.should have_content('Test App')
+      pages.should have_selector('h1', :text => 'Test App')
+   
+    it "should have the right title" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                        :text => "Ruby on Rails Tutorial Sample App | Home")
     end
   end
 
@@ -17,5 +22,26 @@ describe "Static pages" do
       visit '/static_pages/help'
       page.should have_content('Help')
     end
+
+    it "should have the right title" do
+      visit '/static_pages/help'
+      page.should have_selector('title',
+                        :text => "Ruby on Rails Tutorial Sample App | Help")
+    end
   end
+
+  describe "About page" do
+
+    it "should have the content 'About'" do
+      visit '/static_pages/about'
+      page.should have_content('About Us')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/about'
+      page.should have_selector('title',
+                        :text => "Ruby on Rails Tutorial Sample App | About Us")
+    end
+  end
+
 end
